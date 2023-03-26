8c7df9f2c69777edbb088eb742ede82a4a8f06e3
Dirk H. Wolthuis <dirk.wolthuis@ns.nl>
2023-03-26 19:17:40 +0200

    update netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import solid from "solid-start/vite";
-import { defineConfig } from "vite";
+import solid from 'solid-start/vite';
+import { defineConfig } from 'vite';
+import netlify from 'solid-start-netlify';
 
 export default defineConfig({
-  plugins: [solid()],
+	plugins: [solid({ adapter: netlify() })],
 });
