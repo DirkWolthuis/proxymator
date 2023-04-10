570c2ffc1ea68c838953c1ea08e7d203085b7f9b
Dirk H. Wolthuis <dirk.wolthuis@ns.nl>
2023-04-10 13:08:19 +0200

    add login

--- src/root.tsx
+++ src/root.tsx
@@ -1,10 +1,27 @@
 // @refresh reload
-import { Suspense } from 'solid-js';
+import { Show, Suspense, createResource, createSignal } from 'solid-js';
 import { Body, ErrorBoundary, FileRoutes, Head, Html, Meta, Routes, Scripts, Title } from 'solid-start';
 import './root.css';
 import Header from './components/Header';
 
+const checkPassword = async (password: string): Promise<{ valid: boolean }> =>
+	(
+		await fetch(`http://localhost:3000/api/auth`, {
+			method: 'POST',
+			body: JSON.stringify({ password }),
+		})
+	).json();
+
 export default function Root() {
+	const [password, setPassword] = createSignal('');
+	const [auth, setAuth] = createSignal<boolean>();
+
+	const login = async () => {
+		try {
+			const res = await checkPassword(password());
+			setAuth(res.valid);
+		} catch {}
+	};
 	return (
 		<Html lang="en">
 			<Head>
@@ -16,9 +33,30 @@ export default function Root() {
 				<Suspense>
 					<ErrorBoundary>
 						<Header></Header>
-						<Routes>
-							<FileRoutes />
-						</Routes>
+						<Show when={!auth()}>
+							<div class="container">
+								<div class="section">
+									<div class="w-full md:w-1/2 lg:w-1/3">
+										<label htmlFor="password">Password</label>
+										<input
+											name="password"
+											class="mt-2 input"
+											type="password"
+											value={password()}
+											onInput={(e) => setPassword(e.currentTarget.value)}
+										/>
+										<button onClick={() => login()} class="button button-primary mt-4">
+											Login
+										</button>
+									</div>
+								</div>
+							</div>
+						</Show>
+						<Show when={auth()}>
+							<Routes>
+								<FileRoutes />
+							</Routes>
+						</Show>
 					</ErrorBoundary>
 				</Suspense>
 				<Scripts />
