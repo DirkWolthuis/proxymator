e1d03153449ed2bdf39e9fa8d9b49dd40ebd7086
Dirk H. Wolthuis <dirk.wolthuis@ns.nl>
2023-04-21 22:12:29 +0200

    added github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,18 @@
 import { Component } from 'solid-js';
-import { FaBrandsDiscord } from 'solid-icons/fa';
+import { FaBrandsDiscord, FaBrandsGithub } from 'solid-icons/fa';
 
 const Footer: Component<{}> = (props) => {
 	return (
 		<div class="py-4 border-t border-slate-700">
 			<div class="container">
-				<a href={import.meta.env.VITE_DISCORD_LINK}>
-					<FaBrandsDiscord />
-				</a>
+				<div class="flex gap-4">
+					<a href={import.meta.env.VITE_DISCORD_LINK}>
+						<FaBrandsDiscord />
+					</a>
+					<a href={import.meta.env.VITE_GITHUB_LINK}>
+						<FaBrandsGithub />
+					</a>
+				</div>
 			</div>
 		</div>
 	);
