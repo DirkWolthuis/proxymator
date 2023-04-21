21456fdbafc054fc450fa2918c9343abb6685f3f
Dirk H. Wolthuis <dirk.wolthuis@ns.nl>
2023-04-21 22:33:41 +0200

    fix bug and add popperjs

--- src/shared/components/Tag.tsx
+++ src/shared/components/Tag.tsx
@@ -1,5 +1,7 @@
 import { Component, Show, createSignal } from 'solid-js';
 import { FaSolidXmark } from 'solid-icons/fa';
+import usePopper from 'solid-popper';
+
 const Tag: Component<{
 	title: string;
 	value?: any;
@@ -8,22 +10,36 @@ const Tag: Component<{
 	onClose?: (value: any) => any;
 }> = (props) => {
 	const [showTooltip, setShowTooltip] = createSignal(false);
+	const [anchor, setAnchor] = createSignal<HTMLElement | null>();
+	const [popper, setPopper] = createSignal<HTMLElement | null>();
+
+	usePopper(anchor, popper, {
+		placement: 'top',
+		modifiers: [
+			{
+				name: 'offset',
+				options: {
+					offset: [0, 8],
+				},
+			},
+		],
+	});
 
 	return (
 		<div
+			ref={setAnchor}
 			onMouseOver={() => setShowTooltip(true)}
 			onMouseOut={() => setShowTooltip(false)}
 			class="relative mr-2 gap-2 bg-slate-500 items-center h-8 px-3 text-xs mb-2 rounded inline-flex"
 		>
 			<Show when={props.tooltipContent}>
 				<div
+					ref={setPopper}
 					class={`${
 						showTooltip() ? 'visible' : 'invisible'
-					} absolute bottom-[100%] flex justify-center left-[-50%] right-[-50%] pb-1`}
+					} bg-slate-700 pb-1 py-1 rounded-sm flex justify-center`}
 				>
-					<span class="italic text-center bg-slate-700 text-xs px-2 py-1 rounded-sm">
-						{props.tooltipContent}
-					</span>
+					<span class="italic text-center text-xs ">{props.tooltipContent}</span>
 				</div>
 			</Show>
 			<span>{props.title}</span>
